e657fe270283d8c2fa751e982f131f496aa811f8
Vinicius Araujo <vinicius97.lourenco@gmail.com>
2026-04-16 13:47:06 -0300

    1 version

--- home.py
+++ home.py
@@ -70,13 +70,7 @@ try:
             st.plotly_chart(fig_linha, use_container_width=True)
             
     with col_b:
-    # Cria duas colunas internas: uma pequena para a imagem e outra para o texto
-        col_icon, col_txt = st.columns([1, 5])
-    with col_icon:
-        st.image("free_icon_1.svg", width=10)
-    with col_txt:
         st.subheader("Meios de Pagamento")
-    
     if not pagto_df.empty:
             fig_pizza = px.pie(pagto_df, values='qtd', names='metodo_pagamento', hole=0.3)
             fig_pizza.update_layout(margin=dict(l=0, r=0, t=30, b=0), height=300)
