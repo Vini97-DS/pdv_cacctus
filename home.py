63b18954b241fb47f050770939393bbf0b4eee8c
Vinicius Araujo <vinicius97.lourenco@gmail.com>
2026-04-16 13:49:23 -0300

    identacao

--- home.py
+++ home.py
@@ -71,7 +71,7 @@ try:
             
     with col_b:
         st.subheader("Meios de Pagamento")
-    if not pagto_df.empty:
+        if not pagto_df.empty:
             fig_pizza = px.pie(pagto_df, values='qtd', names='metodo_pagamento', hole=0.3)
             fig_pizza.update_layout(margin=dict(l=0, r=0, t=30, b=0), height=300)
             st.plotly_chart(fig_pizza, use_container_width=True)
